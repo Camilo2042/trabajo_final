9f3fc079cefdbeee1e523bee4cce8f6bacc620e2
unknown <camilo2042@hotmail.com>
2022-05-25 19:26:04 -0500

    Creación proyecto, pruebas html

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  {/*<header>
+    <div className="Presentacion">
+      <nav className="Presentacion__1 navbar navbar-expand-lg fixed-top navbar-transparent bg-primary" colorOnScroll="400">
+        <div className="container">
+          <div className="Presentacion__2 navbar-translate">
+            <a className="Presentacion__3 navbar-brand" rel="tooltip">Mi Hoja de vida</a>
+          </div>
+          <ul className="Presentacion__navs navbar-nav">
+            <li className="Presentacion_navs1"><a className="nav-link smooth-scroll" href="#info">Información personal</a></li>
+            <li className="Presentacion_navs2"><a className="nav-link smooth-scroll" href="#habili">Habilidades</a></li>
+            <li className="Presentacion_navs3"><a className="nav-link smooth-scroll" href="#expe">Experiencia - Estudios</a></li>
+            <li className="Presentacion_navs4"><a className="nav-link smooth-scroll" href="#contact">Contactame</a></li>
+          </ul>
+        </div>
+      </nav>
     </div>
+  </header>*/}
   );
 }
 
